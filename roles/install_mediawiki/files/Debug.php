d8bff3412ac3766553e425663300b9add4969de9
Cindy Cicalese <cicalese@mitre.org>
2016-06-28 21:01:39 -0400

    Updated extensions and reworked config.
    Change-Id: I2e0b9ec09676aca78138edf13b6f80ac3874d53d

--- roles/install_mediawiki/files/Debug.php
+++ roles/install_mediawiki/files/Debug.php
@@ -1,5 +1,7 @@
 <?php
 
-#$wgResourceLoaderDebug = false;
+#error_reporting( -1 );
+#ini_set( 'display_errors', 1 );
+#$wgResourceLoaderDebug = true;
 #$wgShowExceptionDetails = true;
 #$wgDebugLogFile= "/tmp/MediaWikiDebug.log";
