6635e567f3d2ba329c11d550b1eb051d42fafe2d
cicalese <cicalese@mitre.org>
2016-12-05 14:22:56 -0500

    Updated extensions; Fixed logo issue.
    Change-Id: Ic941cb099b253629442a4384099667d59479c36e

--- roles/install_mediawiki/files/Debug.php
+++ roles/install_mediawiki/files/Debug.php
@@ -3,5 +3,5 @@
 #error_reporting( -1 );
 #ini_set( 'display_errors', 1 );
 #$wgResourceLoaderDebug = true;
-#$wgShowExceptionDetails = true;
+#$wgShowDBErrorBacktrace = true;
 #$wgDebugLogFile= "/tmp/MediaWikiDebug.log";
